4506f212e8a2939f88250b753dbdfbe7b9afab1b
franzp <pobocekfrantisek@gmail.com>
2020-05-22 01:52:44 +0200

    Server scaffolding

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# H-P
+# Hackers & painters
+
+Server - Nodejs, Express, Typescript, Redis, PostgresSQL, Swagger (docs)
+
+Base route: /api/{version}/{route}
+
+Tests files - {filename}.test.{extension}
---
+++ server/src/services/search/routes.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from 'express';
+import { checkSearchParams } from '../../middleware/checks';
+
+export default [
+  {
+    /**
+     * @swagger
+     * /search:
+     *  get:
+     *    description: use to search something
+     *    parameters:
+     *      - name: q
+     *        description: whatever
+     *        in: query
+     *        required: true
+     *        type: string
+     *    responses:
+     *      '200':
+     *        description: A successful response
+     */
+    path: '/search',
+    method: 'get',
+    handler: [
+      checkSearchParams,
+      async ({ query }: Request, res: Response) => {
+        console.log(query.q);
+        res.send('Hello world!');
+      }
+    ]
+  }
+];
