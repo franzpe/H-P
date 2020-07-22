08843e19cbc1332bbf2a2795315fb7a1f6de9847
franzp <pobocekfrantisek@gmail.com>
2020-07-22 21:01:23 +0200

    Client-landing - scroll behaviour polyfill

--- client-landing/pages/_app.tsx
+++ client-landing/pages/_app.tsx
@@ -1,12 +1,17 @@
 import { AppProps } from 'next/app';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 
 import { CustomNextComponentType } from '../types/common';
 import SiteLayout from '../components/layouts/SiteLayout';
 
 import '../styles/index.css';
+import { initializePolyfills } from '../libs/pollyfills';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
+  useEffect(() => {
+    initializePolyfills();
+  }, []);
+
   const getLayout = (Component as CustomNextComponentType).getLayout || (page => <SiteLayout>{page}</SiteLayout>);
   return getLayout(<Component {...pageProps} />);
 };
